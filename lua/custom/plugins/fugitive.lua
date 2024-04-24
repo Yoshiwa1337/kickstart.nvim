67d6a164f1498dabc6277d95d3157a013ed94c51
Yoshi <forbes.y2403@gmail.com>
2024-04-24 14:57:38 +0100

    Added keymaps to fugitive

--- lua/custom/plugins/fugitive.lua
+++ lua/custom/plugins/fugitive.lua
@@ -8,4 +8,13 @@ return {
   vim.keymap.set('n', '<leader>gp', function()
     vim.cmd.Git 'push'
   end),
+
+  vim.keymap.set('n', '<leader>gP', function()
+    vim.cmd.Git { 'pull', '--rebase' }
+  end),
+
+  vim.keymap.set('n', '<leader>gt', ':Git push -u origin '),
+
+  vim.keymap.set('n', '<leader>gl', '<cmd>diffget //2<CR>'),
+  vim.keymap.set('n', '<leader>gr', '<cmd>diffget //3<CR>'),
 }
